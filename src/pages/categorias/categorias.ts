f7cca3141cee57e5b445b4683f3099774bd8da6d
LuizC3s4r1o <luiz.cesario@yahoo.com.br>
2018-11-26 23:21:56 -0200

    Criando um interceptor para tratamento de erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -26,10 +26,8 @@ export class CategoriasPage {
       .subscribe(response => {
         this.items = response;
       },
-      error => {
-        console.log(error);
-      }
-    )
+      error => {});
+
   }
 
 }
